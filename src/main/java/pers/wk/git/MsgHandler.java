53368cc7793bff62c4104437f51d5c9b437de52f
wuke5 <wuke5@xiaomi.com>
2019-06-08 22:27:03 +0800

    “新增俩测试文件”

---
+++ src/main/java/pers/wk/git/MsgHandler.java
@@ -0,0 +1,13 @@
+package pers.wk.git;
+
+/**
+ * @description:
+ * @author: wuke
+ * @create: 2019-06-08 22:26
+ **/
+public class MsgHandler {
+
+    public static void handle(String input) {
+        System.out.println("handle");
+    }
+}
---
+++ src/main/java/pers/wk/git/Test.java
@@ -0,0 +1,14 @@
+package pers.wk.git;
+
+/**
+ * @description:
+ * @author: wuke
+ * @create: 2019-06-08 15:30
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("test");
+    }
+}
